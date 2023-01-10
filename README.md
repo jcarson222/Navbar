c89dd61fa93f6b46498bda8e8cd4f6faf09bedf4
Jesse Carson <jmcarson222@gmail.com>
2023-01-10 13:41:41 -0600

    add funtionality
    add README description

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Navbar
+# Responsive Navbar
+Toggle the navbar on/off when the screen is resized to min-width: 800px.
--- app.js
+++ app.js
@@ -3,3 +3,20 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+// on click, if links botton has the "show-links" class => remove it, if it does not => add it.
+
+navToggle.addEventListener("click", () => {
+    // USING IF/ELSE
+    // if (links.classList.contains("show-links")) {
+    //     links.classList.remove("show-links");
+    // } else {
+    //     links.classList.add("show-links");
+    // }
+
+    // USING THE TOGGLE METHOD
+    links.classList.toggle("show-links");
+    });
